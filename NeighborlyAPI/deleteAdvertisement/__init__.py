9736ab5f8b1bbd79d87d9283c4975447df34231f
Ian Mungovan <57017900+ITM007@users.noreply.github.com>
2021-07-09 14:27:58 -0400

    First Try

--- NeighborlyAPI/deleteAdvertisement/__init__.py
+++ NeighborlyAPI/deleteAdvertisement/__init__.py
@@ -9,9 +9,9 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
     if id:
         try:
-            url = "mongodb://project2cosmodb:ldsE8YCzgTi8GFfR84wCimXVrZCLbrLbUXfj2uwVQKRB2kgyaLC7ZArYoKl1nhSYURsamm90fOZlhRqk9BnXoA==@project2cosmodb.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@project2cosmodb@"  # TODO: Update with appropriate MongoDB connection information
+            url = "mongodb://iandbp2:Jtjt2LUoLaukoMUTtjGdtjhFg1VH6DBDHpNhbQT7cgjdK12PALg2oEZ9ymGlKe0rFBAs7U1OflJpZ71bxfTasw==@iandbp2.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@iandbp2@"  # TODO: Update with appropriate MongoDB connection information
             client = pymongo.MongoClient(url)
-            database = client["project2db"]
+            database = client["neighborapp"]
             collection = database["advertisements"]
 
             query = {"_id": ObjectId(id)}
